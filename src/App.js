06541f614cdddf2537698c0925caaaf1cbf1e8eb
emnakzr <emna.kanzari@isticbc.org>
2022-04-10 10:57:32 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <><Header /><Footer /></>
+  );
+}
+
+export default App;
